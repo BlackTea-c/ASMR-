90b3585dc69f81fac8e2013b760ce412f70f881d
BlackTea-c <2286554510@qq.com>
2024-03-21 20:56:41 +0800

    1

--- 相关NLP基础/1.py
+++ 相关NLP基础/1.py
@@ -1,1 +1,17 @@
 # 处理文本首先词嵌入  就是把原本离散的文本词 映射到实数域！
+
+
+import collections
+import math
+import random
+import sys
+import time
+import os
+import numpy as np
+import torch
+from torch import nn
+import torch.utils.data as Data
+
+sys.path.append("..")
+
+print(torch.__version__)
